517aef9ff7659fc0c231bd679a8b7578bc6e4012
Alex Rao <alexhrao@gmail.com>
2017-01-01 14:16:43 -0600

    Change from ThreadGroups to Game.
    ThreadGroups are basically deprecated, so we need to kind of create our own class for handling different games.

--- Server/src/main/java/networking/Game.java
+++ Server/src/main/java/networking/Game.java
@@ -3,13 +3,20 @@ package main.java.networking;
 /**
  * This class will be responsible for keeping games separate. It will do (most) of what the server currently does now!
  */
-public class Game extends ThreadGroup {
+public class Game {
+    /*
+    Basically, this class should be a "mini-server" - a server for a single game. Each Game instance will be
+    centered around 1 single game - and it's name should be the name of the player that started the game! (And number
+    of free spaces? I don't know - perhaps toString Method can handle this!). From now on, a SudokuPacket that is
+    requesting the server needs to either a) provide the number of spaces, b) specify which Game it wants to join, or
+    c) query what Games are currently active.
+    a)
+        If it provides the number of spaces, then it wants to create a new game. We should create a Game with the name
+        of the player as the creator, and create the new gameboard, etc.
+    b)
+        If it provides the Game it wishes to join, then the conversation (The Socket) should be passed to that game.
+    c)
+        If it queries the current Games, then it should return with a list of games and their identifiers.
 
-    public Game(String name) {
-        super(name);
-    }
-
-    public Game(ThreadGroup group, String name) {
-        super(group, name);
-    }
+     */
 }
