48c133eee284f55c9e33e86956b251ecdd3f7996
Alex Rao <alexhrao@gmail.com>
2017-01-01 22:54:31 -0600

    Fix bug where clicking square did not do anything.

--- Client/src/main/java/ui/GameUI.java
+++ Client/src/main/java/ui/GameUI.java
@@ -36,7 +36,7 @@ public class GameUI extends BorderPane {
             }
         }
         info = new InfoMenu(this);
-        StackPane center = new StackPane(board, squares);
+        StackPane center = new StackPane(squares, board);
         chat = new Chat(control);
         chatter = new Chatter();
         chatter.getSender().setOnAction(e -> {
