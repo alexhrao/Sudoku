7efb46d7a996226b60c323e7dc9c8ee457e00376
Alex Rao <alexhrao@gmail.com>
2017-01-02 11:15:55 -0600

    Add installer files.

---
+++ installers/EXE/buildinstall.cmd
@@ -0,0 +1,11 @@
+rem Ensure Launch4j is installed and is in your path
+rem Hit enter when ready to start ...
+pause
+launch4jc sudokualex.xml
+rem Ensure EXE file with current date / time was created in installation folder
+pause
+rem Ensure InnoSetup is in path
+iscc /O+ sudokualex.iss
+rem Check to ensure installation file has been created
+pause
+exit
