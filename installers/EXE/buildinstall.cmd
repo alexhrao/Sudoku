2e064e4e9e7a54e70e57d403d684968711b1bfe6
Alexander Rao <alexhrao@gmail.com>
2017-06-25 13:40:35 -0400

    Update EXE installer script and create new EXE installer

--- installers/EXE/buildinstall.cmd
+++ installers/EXE/buildinstall.cmd
@@ -1,12 +1,11 @@
 @ECHO off
+copy ..\..\out\artifacts\SudokuPlayer\SudokuPlayer.jar .
+copy ..\..\out\artifacts\SudokuAlexServer\SudokuAlexServer.jar .
 rem Ensure Launch4j is installed and is in your path
 rem Hit enter when ready to start ...
-pause
 launch4jc sudokualex.xml
 rem Ensure EXE file with current date / time was created in installation folder
-pause
 rem Ensure InnoSetup is in path
 iscc /O+ sudokualex.iss
 rem Check to ensure installation file has been created
-pause
 exit
